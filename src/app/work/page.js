89e096dc473caf5dd870fcf4fbaf7d3416063699
matildasutho <matildasutherland23@gmail.com>
2025-07-21 14:31:55 +1000

    linting errors.

--- src/app/work/page.js
+++ src/app/work/page.js
@@ -2,7 +2,7 @@ export default function Work() {
   return (
     <div className="work">
       <h1>Our Work</h1>
-      <p>Explore our portfolio to see the projects we've completed.</p>
+      <p>Explore our portfolio to see the projects we&apos;ve completed.</p>
       <p>Contact us for more information!</p>
     </div>
   );
--- src/components/home/AboutSection.jsx
+++ src/components/home/AboutSection.jsx
@@ -3,21 +3,24 @@ import PrimaryButton from "@/components/ui/PrimaryButton";
 
 export default function AboutSection() {
   return (
-    <section className="flex items-center  border-b-1 border-[var(--mesm-grey)] pb-[var(--global-margin-lg)]">
+    <section className="flex items-center border-b-1 border-[var(--mesm-grey)] pb-[var(--global-margin-lg)]">
       <StaticTwoColumn
-        label={"what we're about"}
-        column1={[
-          <h1 key="title" className="text-md md:text-6xl">
-            Not just another marketing agency.
-          </h1>,
-          <p key="desc" className="">
-            We blend smart data with bold design to deliver work that doesn’t
-            just look good, it performs.
-          </p>,
-          <PrimaryButton key="button" href="/connect" size="large">
-            Connect
-          </PrimaryButton>,
-        ]}
+        label="what we're about"
+        column1={
+          <>
+            <h1 className="text-md md:text-6xl">
+              Not just another marketing agency.
+            </h1>
+            <p>
+              We blend smart data with bold design to deliver work that
+              doesn&apos;t just look good, it performs.
+            </p>
+            <PrimaryButton href="/connect" size="large">
+              Connect
+            </PrimaryButton>
+          </>
+        }
+        column2={null}
       />
     </section>
   );
