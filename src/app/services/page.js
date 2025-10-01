f1c4fe3c09ad7dbb5cc9fdb769b3719b37fcd152
matildasutho <matildasutherland23@gmail.com>
2025-10-01 14:35:38 +1000

    update services FAQ

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,35 +1,33 @@
 "use client";
 
-import StaticTwoColumn from "@/components/layout/StaticTwoColumn";
 import StaticSingleColumn from "@/components/layout/StaticSingleColumn";
-import Button from "@/components/ui/Button";
+
 import FAQ from "@/components/layout/FAQ";
 import StaggeredWords from "@/hooks/StaggeredWords";
 import ServicesRail from "@/components/services/ServicesRail";
-import SteppedAccordion from "@/components/layout/SteppedAccordion";
 
 export default function Connect() {
   const servicesFAQ = [
     {
       question:
-        "What’s the difference between Defined and Perpetual Collaboration?",
+        "What’s the difference between Defined and Continuous Collaboration?",
       textContent:
-        "Defined Collaboration is perfect for projects with a clear scope, timeline, and deliverables. Ideal for businesses looking for a fixed-price, time-bound engagement.<br /><br />Perpetual Collaboration is designed for long-term partnerships with flexibility and scalability, providing ongoing strategic, creative, and performance support as your needs evolve.",
+        "Defined Collaboration is perfect for projects with a clear scope, timeline, and deliverables. Ideal for businesses looking for a fixed-price, time-bound engagement.<br/><br/>Continuous Collaboration is designed for long-term partnerships with flexibility and scalability, providing ongoing strategic, creative, and performance support to keep you at the forefront of your industry.",
     },
     {
       question: "What is Mesmerise’s unique strength as an agency?",
       textContent:
-        "Our strength lies in our holistic, integrated approach across Strategy, Design, Growth Marketing, and Business Consulting.<br /><br />We don’t just execute, we carefully construct systems that align every touchpoint of your digital presence with your business goals, delivering growth that compounds.",
+        "Our strength lies in our holistic, integrated approach across Strategy, Design, Omnipresent Marketing, and Business Consulting.<br/><br/>We don’t just “run ads”, we carefully construct ecosystems that elevate your market position. This allows you to charge more, lower your cost per acquisition and extend the lifetime value of your clients. ",
     },
     {
       question: "What type of clients do you typically work with?",
       textContent:
-        "We collaborate with ambitious SMEs, premium B2B brands, SaaS & technology companies, and enterprise-level organisations.<br /><br />Our experience enables us to navigate both the agility of startups and the complexity of large corporate environments.",
+        "We collaborate with ambitious SMEs, premium B2B brands, SaaS & technology companies, and enterprise-level organisations.<br/><br/>Our approach enables us to navigate both the agility of startups and the complexity of large corporate environments.",
     },
     {
       question: "What does the typical project process look like?",
       textContent:
-        "We start with a discovery session to understand your goals, challenges, and priorities. Then we recommend the right engagement model, assemble the right team, create a roadmap, and execute with clear milestones.",
+        "It starts with a chat. The first step is a discovery session to understand your goals, challenges, and priorities. Then we recommend the right engagement model, create a roadmap, and execute with clear milestones.",
     },
     {
       question: "How flexible are your engagement models?",
@@ -39,12 +37,12 @@ export default function Connect() {
     {
       question: "What kind of project management setup do you offer?",
       textContent:
-        "You’ll have a dedicated project lead as your single point of contact, supported by real-time dashboards for transparency on progress, resource allocation, and KPIs.<br /><br />Defined Collaborations are fixed and predictable, while Perpetual Collaborations are billed monthly, with room to adjust as needed.",
+        "We’re a small tight knit team which means you deal directly with the people responsible for the execution.<br/><br/>That means fewer delays, no miscommunication, and lighting fast momentum.<br/><br/><ul><li>Petar & Nicole shape the strategy with you and make sure every decision drives revenue forward.</li><li>Matilda takes the lead on design and development, turning ideas into assets that perform.</li><li>Simba handles the business development and consulting, aligning projects with your long-term goals.</li></ul><br/><br/>We set clear milestones, give you regular updates, and adapt touchpoints around your preferences. The level of communication is tailored to your project and level of need.<br/><br/>This way you get the best of both worlds: the responsiveness and personal attention of a intimate team, with the professionalism and accountability of a boutique agency.",
     },
     {
       question: "What results can I expect from working with Mesmerise?",
       textContent:
-        "We bring measurable impact to every engagement. For instance REIGNER had to open another business to meet fulfillment, Tony Caulk hired 4+ new staff and a full time admin person, the list goes on. 97% of small businesses we work with can’t handle the lead volume and don’t have systems in place to handle the growth engine we’ve implemented.",
+        "It depends on your industry and niche. Organise a strategy session and we’ll tell you exactly what you can expect.<br/><br/>For instance, REIGNER had to open another business to meet fulfillment, Tony Caulk hired 4+ new staff and a full time admin person, the list goes on.<br/><br/>90% of businesses we work with can’t handle the lead volume we are able to generate.",
     },
   ];
 
