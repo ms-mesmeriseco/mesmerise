f4debb86ae415b7a76b64aaeea7eab4a88144405
matildasutho <matildasutherland23@gmail.com>
2025-10-03 15:27:03 +1000

    lint

--- src/app/form-success/page.js
+++ src/app/form-success/page.js
@@ -3,10 +3,8 @@ import PageTitleLarge from "@/components/layout/PageTitleLarge";
 export default function Success() {
   return (
     <div>
-      <PageTitleLarge text={"Thank you!"} />
-      <div className="page-title-medium md:w-1/2">
-        We've recieved your email. Someone will be in touch with you shortly.
-      </div>
+      <PageTitleLarge text={"Thank you"} />
+      <div className="p3 md:w-1/2">{"We'll be in touch with you shortly."}</div>
     </div>
   );
 }
