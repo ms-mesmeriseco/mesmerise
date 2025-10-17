b4ce818855128a5a75ad9b8c459174da775329b6
matildasutho <matildasutherland23@gmail.com>
2025-10-17 13:37:53 +1100

    update footer link

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -41,7 +41,10 @@ export default function Footer() {
           <div className="flex flex-col gap-4">
             <div className="flex items-end gap-3">
               {/* Update the src to your actual wordmark path */}
-              <Link href="">
+              <Link
+                href="www.mesmeriseco.com"
+                aria-label="Mesmerise Digital Home"
+              >
                 <Image
                   src="/wordmark.svg"
                   alt="Mesmerise Digital"
