63fff6315848e0ba819024bb563815bc66c50075
matildasutho <matildasutherland23@gmail.com>
2025-10-21 16:45:40 +1100

    blog draft page

--- src/components/cms-blocks/BlogNav.jsx
+++ src/components/cms-blocks/BlogNav.jsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { getClient } from "@/lib/apollo-client";
 import { GET_ALL_BLOG_POSTS } from "@/lib/graphql/queries/getBlogPosts";
 
-export default function BlogThreeColumn() {
+export default function BlogNav() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -22,9 +22,9 @@ export default function BlogThreeColumn() {
   }, []);
 
   return (
-    <section className="w-full py-[var(--global-margin-lg)] px-[var(--global-margin-sm)]">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-[var(--global-margin-sm)]">
-        {posts.slice(0, 3).map((post, index) => (
+    <section className="w-full">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-[var(--global-margin-sm)]">
+        {posts.slice(0, 4).map((post, index) => (
           <Link
             key={`blog-${post.slug}-${index}`}
             href={`/blog/${post.slug}`}
@@ -37,7 +37,31 @@ export default function BlogThreeColumn() {
                 alt={post.heroImage.title || "Blog image"}
                 width={post.heroImage.width || 800}
                 height={post.heroImage.height || 600}
-                className="object-cover w-full rounded-md "
+                className="object-cover w-full rounded-md aspect-6/4"
+                style={{ display: "block" }}
+              />
+            )}
+
+            {/* Text below image */}
+            <div className="py-6">
+              <h4 className="text-sm font-bold">{post.postTitle}</h4>
+            </div>
+          </Link>
+        ))}
+        {posts.slice(5, 9).map((post, index) => (
+          <Link
+            key={`blog-${post.slug}-${index}`}
+            href={`/blog/${post.slug}`}
+            className="group overflow-hidden border-[var(--foreground)] duration-200 flex flex-col h-fit"
+          >
+            {/* Image */}
+            {post.heroImage?.url && (
+              <Image
+                src={post.heroImage.url}
+                alt={post.heroImage.title || "Blog image"}
+                width={post.heroImage.width || 800}
+                height={post.heroImage.height || 600}
+                className="object-cover w-full rounded-md aspect-6/4"
                 style={{ display: "block" }}
               />
             )}
