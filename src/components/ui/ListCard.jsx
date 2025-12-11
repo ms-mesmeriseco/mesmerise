98d189df08b5642cf566bc5afbe495e685d0656e
matildasutho <matildasutherland23@gmail.com>
2025-12-11 12:16:47 +1100

    restyle cards

--- src/components/ui/ListCard.jsx
+++ src/components/ui/ListCard.jsx
@@ -15,14 +15,14 @@ export default function ListCard({
         translateX: "2px",
       }}
       transition={{ damping: 20, duration: 0.1 }}
-      className={`flex md:flex-row flex-col md:max-w-3xl gap-6 md:items-center items-start bg-[var(--mesm-grey-xd)] border-1 border-[var(--mesm-grey-dk)]/40 md:py-4 py-8 px-8 rounded-md shadow hover:border-[var(--mesm-grey-dk)] duration-200`}
+      className={`flex md:flex-row flex-col md:max-w-3xl gap-6 md:items-center items-start bg-[var(--mesm-grey-xd)] border-1 border-[var(--mesm-grey-dk)] md:py-4 py-8 px-8 rounded-lg shadow hover:border-[var(--mesm-grey)] duration-100`}
       {...props}
     >
       {icon && (
         <img
           src={icon}
           alt={entryTitle || ""}
-          className="w-10 h-10 object-contain"
+          className="w-10 h-10 object-contain fill-[var(--mesm-blue)]"
         />
       )}
       <div className="prose">{children}</div>
