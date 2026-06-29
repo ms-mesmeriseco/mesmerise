36abc7a7202902440164ec4cc437aade4958955c
matildasutho <matildasutherland23@gmail.com>
2026-06-29 10:47:40 +1000

    updat elayout metadata to next in built helper

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,50 +12,52 @@ import Script from "next/script";
 import HeroLoader from "@/components/three/HeroLoader";
 import { HeroLoaderProvider } from "@/components/three/HeroLoaderProvider";
 
-export const metadata = {
-  title: "Mesmerise Digital",
-  description: "We don't just run ads.",
-  alternates: {
-    canonical: `https://www.mesmeriseco.com`,
-  },
-  openGraph: {
+export async function generateMetadata() {
+  return {
     title: "Mesmerise Digital",
     description: "We don't just run ads.",
-    url: "https://www.mesmeriseco.com",
-    siteName: "Mesmerise Digital",
-    images: [
-      {
-        url: `https://www.mesmeriseco.com/assets/social-default.png`,
-        width: 1200,
-        height: 630,
-        alt: "We don't just run ads.",
-      },
-    ],
-    locale: "en_AU",
-    type: "article",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Mesmerise Digital",
-    description: "We don't just run ads.",
-    images: `https://www.mesmeriseco.com/assets/social-default.png`,
-  },
-  icons: {
-    // Browser tab favicons
-    icon: [
-      { url: "favicon-32x32.png", sizes: "any" }, // multi-size .ico (16/32)
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
-    // (Optional) Safari pinned tab (monochrome SVG) — looks great in macOS
-    other: [
-      { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#111111" },
-    ],
-  },
-};
+    alternates: {
+      canonical: `https://www.mesmeriseco.com`,
+    },
+    openGraph: {
+      title: "Mesmerise Digital",
+      description: "We don't just run ads.",
+      url: "https://www.mesmeriseco.com",
+      siteName: "Mesmerise Digital",
+      images: [
+        {
+          url: `https://www.mesmeriseco.com/assets/social-default.png`,
+          width: 1200,
+          height: 630,
+          alt: "We don't just run ads.",
+        },
+      ],
+      locale: "en_AU",
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Mesmerise Digital",
+      description: "We don't just run ads.",
+      images: `https://www.mesmeriseco.com/assets/social-default.png`,
+    },
+    icons: {
+      // Browser tab favicons
+      icon: [
+        { url: "favicon-32x32.png", sizes: "any" }, // multi-size .ico (16/32)
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      ],
+      apple: [
+        { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      ],
+      // (Optional) Safari pinned tab (monochrome SVG) — looks great in macOS
+      other: [
+        { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#111111" },
+      ],
+    },
+  };
+}
 export const viewport = {
   width: "device-width",
   initialScale: 1,
