d67682cd779a5001d331ba3f0d8aa92f1711a743
matildasutho <matildasutherland23@gmail.com>
2026-08-21 09:52:35 +1000

    fix title for collab model

--- src/app/collaboration/[mode]/page.js
+++ src/app/collaboration/[mode]/page.js
@@ -60,5 +60,11 @@ export async function generateMetadata({ params }) {
         },
       ],
     },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: DEFAULT_OG_IMAGE_URL,
+    },
   };
 }
